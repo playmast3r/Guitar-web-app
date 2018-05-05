f1aa78d0073e1341f8e881bc53bd5d6a5e447bc6
Siddharth Mahajan <playmast3r@users.noreply.github.com>
2018-05-05 22:10:35 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    $DB_SERVER = 'IP';
+    $DB_USERNAME = 'username';
+    $DB_PASSWORD = 'password';
+    $DB_DATABASE = 'db';
+    $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
+?>
